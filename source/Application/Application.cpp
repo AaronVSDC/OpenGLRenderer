48bcaacbe86e8405e63bc9efb8cfc6fd20b97191
Aaron_Van_Sichem_De_Combe <aaronvsdc@gmail.com>
2025-10-20 01:58:44 +0200

    Working camera, but code structure/quality is a hot pile of garbage
    luckily we still have time to refactor

--- source/Application/Application.cpp
+++ source/Application/Application.cpp
@@ -2,23 +2,40 @@
 #include "Window.h"
 #include "InputManager.h"
 #include "glad/glad.h"
-#include <iostream>
 #include "OpenGLShader.h"
 #include "stb_image.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <SDL3/SDL_mouse.h>
+
+
 
 namespace Papyrus
 {
+    // Camera variables
+    static glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    static glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    static glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    static bool firstMouse = true;
+    static float yaw = -90.0f;
+    static float pitch = 0.0f;
+    static float lastX = 400.0f;
+    static float lastY = 300.0f;
+    static float fov = 45.0f;
+
+    static float deltaTime = 0.0f;
+    static float lastFrame = 0.0f;
+
     Application::Application()
     {
         m_Window = std::make_unique<Window>("Portal!!", 800, 600);
+        SDL_SetWindowRelativeMouseMode(m_Window->getHandle(), true); 
     }
 
-    Application::~Application()
-    {
-    }
+    Application::~Application() {}
 
     void Application::run()
     {
@@ -100,7 +117,7 @@ namespace Papyrus
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
 
-        // Load first texture
+        // Textures
         unsigned int texture1, texture2;
         glGenTextures(1, &texture1);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -117,30 +134,22 @@ namespace Papyrus
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
         }
-        else
-        {
-            std::cerr << "Failed to load texture 1" << std::endl;
-        }
+        else std::cerr << "Failed to load texture 1" << std::endl;
         stbi_image_free(data);
 
-        // Load second texture
         glGenTextures(1, &texture2);
         glBindTexture(GL_TEXTURE_2D, texture2);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
         data = stbi_load("Resources/Textures/awesomeface.png", &width, &height, &nrChannels, 0);
         if (data)
         {
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
         }
-        else
-        {
-            std::cerr << "Failed to load texture 2" << std::endl;
-        }
+        else std::cerr << "Failed to load texture 2" << std::endl;
         stbi_image_free(data);
 
         shaders->bind();
@@ -150,8 +159,69 @@ namespace Papyrus
         bool doContinue = true;
         while (doContinue)
         {
-            doContinue = inputManager.processInput();
+            float currentFrame = SDL_GetTicks() / 1000.0f;
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
 
+            // Handle SDL input
+            SDL_Event e;
+            while (SDL_PollEvent(&e))
+            {
+                if (e.type == SDL_EVENT_QUIT) doContinue = false;
+                if (e.type == SDL_EVENT_WINDOW_RESIZED)
+                    glViewport(0, 0, e.window.data1, e.window.data2);
+
+                if (e.type == SDL_EVENT_MOUSE_MOTION)
+                {
+                    float xpos = static_cast<float>(e.motion.x);
+                    float ypos = static_cast<float>(e.motion.y);
+
+                    if (firstMouse)
+                    {
+                        lastX = xpos;
+                        lastY = ypos;
+                        firstMouse = false;
+                    }
+
+                    float xoffset = xpos - lastX;
+                    float yoffset = lastY - ypos;
+                    lastX = xpos;
+                    lastY = ypos;
+
+                    float sensitivity = 0.1f;
+                    xoffset *= sensitivity;
+                    yoffset *= sensitivity;
+
+                    yaw += xoffset;
+                    pitch += yoffset;
+
+                    if (pitch > 89.0f) pitch = 89.0f;
+                    if (pitch < -89.0f) pitch = -89.0f;
+
+                    glm::vec3 front;
+                    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+                    front.y = sin(glm::radians(pitch));
+                    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+                    cameraFront = glm::normalize(front);
+                }
+
+                if (e.type == SDL_EVENT_MOUSE_WHEEL)
+                {
+                    fov -= (float)e.wheel.y;
+                    if (fov < 1.0f) fov = 1.0f;
+                    if (fov > 45.0f) fov = 45.0f;
+                }
+            }
+
+            const bool* state = SDL_GetKeyboardState(nullptr); 
+            float cameraSpeed = 2.5f * deltaTime;
+            if (state[SDL_SCANCODE_W]) cameraPos += cameraSpeed * cameraFront;
+            if (state[SDL_SCANCODE_S]) cameraPos -= cameraSpeed * cameraFront;
+            if (state[SDL_SCANCODE_A]) cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+            if (state[SDL_SCANCODE_D]) cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+            if (state[SDL_SCANCODE_ESCAPE]) doContinue = false;
+
+            // Render
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -159,11 +229,10 @@ namespace Papyrus
             glBindTexture(GL_TEXTURE_2D, texture1);
             glActiveTexture(GL_TEXTURE1);
             glBindTexture(GL_TEXTURE_2D, texture2);
-
             shaders->bind();
 
-            glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-            glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+            glm::mat4 projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+            glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
             shaders->uploadUniformMat4("projection", projection);
             shaders->uploadUniformMat4("view", view);
 
@@ -175,7 +244,6 @@ namespace Papyrus
                 float angle = 20.0f * i;
                 model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
                 shaders->uploadUniformMat4("model", model);
-
                 glDrawArrays(GL_TRIANGLES, 0, 36);
             }
 
